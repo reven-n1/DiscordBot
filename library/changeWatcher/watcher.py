48dbbeaaef1024a9456edcd3628262d2b0d46b78
reven-n1 <roma.suslov.16@gmail.com>
2021-09-19 21:22:58 +0300

    prepared event handler class for changewatcher

--- library/changeWatcher/watcher.py
+++ library/changeWatcher/watcher.py
@@ -1,44 +1,34 @@
-from watchdog.events import FileSystemEventHandler
-from watchdog.events import LoggingEventHandler
-from watchdog.observers import Observer
+from importlib import reload 
+import watchdog.observers
+import watchdog.events
 import asyncio
-import logging
-import time
+
+
+class Handler(watchdog.events.PatternMatchingEventHandler):
+    def __init__(self):
+        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=["*.py", "*.json"], ignore_patterns = None,
+                                                     ignore_directories = False, case_sensitive = True)
+
+    def on_created(self, event):
+        print(f"File was created at {event.src_path}")
+        
+
+    def on_deleted(self, event):
+        print(f"File was deleted at {event.src_path}")
+
+    
+
+    def on_modified(self, event):
+        print(f"File was modified at {event.src_path}")
 
 
 async def main():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
-    )
-
-    path = "/home/reven_n1/Projects/Python/Bots/DiscordBot/library"
-
-    even_handler = LoggingEventHandler()
-
-    observer = Observer()
-    observer.schedule(even_handler, path, recursive=True)
-
+    event_handler = Handler()
+    observer = watchdog.observers.Observer()
+    observer.schedule(event_handler, r"/home/reven_n1/Projects/Python/Bots/DiscordBot/library", recursive = True)
     observer.start()
-
-    try:
-        while True:
-            await asyncio.sleep(5)
-    except KeyboardInterrupt:
-        observer.stop()
-    
     observer.join()
-
-# class Handler(FileSystemEventHandler):
-#     def on_created(self, event):
-#         print event
-
-#     def on_deleted(self, event):
-#         print event
-
-#     def on_moved(self, event):
-#         print event
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
